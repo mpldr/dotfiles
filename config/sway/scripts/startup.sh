5059595d647eca84dad4b7334a3fdb0d905cfd17
Moritz Poldrack <git@moritz.sh>
2022-04-08 11:31:39 +0200

    sway: added weechat to autostart

--- config/sway/scripts/startup.sh
+++ config/sway/scripts/startup.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-ydotool key alt+2
-sleep 0.1
-ydotool key alt+left
-sleep 0.1
-ydotool key alt+8
-sleep 0.2
-kitty --class=org-mode aerc &
-sleep 0.1
-kitty --class=org-mode calcurse &
+sleep 1
+/bin/sh -c 'while true; do kitty --class=org-mode aerc; notify-send "aerc quit!" "Status: "$?; done' &
+/bin/sh -c 'while true; do kitty --class=org-mode calcurse; notify-send "calcurse quit!" "Status: "$?; done' &
+/bin/sh -c 'while true; do kitty --class=comm-terminal weechat; notify-send "weechat quit!" "Status: "$?; done' &
