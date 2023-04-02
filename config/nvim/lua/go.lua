ec9e49f65bcf42dc2d6b5863782ed4c2b5d42816
Moritz Poldrack <git@moritz.sh>
2023-04-02 22:28:47 +0200

    nvim: remove old config

--- config/nvim/lua/go.lua
+++
@@ -1,7 +0,0 @@
-vim.g['go_fmt_command'] = "gopls"
-vim.g['go_gopls_gofumpt'] = 1
-vim.g['go_auto_type_info'] = 1
-
--- Custom keybindings from hell
-vim.keymap.set('n', 'gie', '<Cmd>GoIfErr<CR>' )
-vim.keymap.set('n', 'gis', '<Cmd>GoFillStruct<CR>' )
